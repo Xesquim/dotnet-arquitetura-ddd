6b7e1fce046ecead34c61d6594e482138de9b8da
Xesquim <matheus.g.cesquim@gmail.com>
2023-06-28 01:15:01 -0300

    Creating zip code controller

---
+++ src/Api.Application/Controllers/ZipCodeController.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Dtos.ZipCode;
+using Api.Domain.Interfaces.Services.ZipCode;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ZipCodeController : ControllerBase
+    {
+        public IZipCodeService _service { get; set; }
+        
+        public ZipCodeController(IZipCodeService service)
+        {
+            _service = service;
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("byId/{id}", Name = "GetZipCodeWithId")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            try
+            {
+                var result = await _service.Get(id);
+                if(result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("{zipCode}")]
+        public async Task<ActionResult> Get(string zipCode)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            try
+            {
+                var result = await _service.Get(zipCode);
+                if(result == null)
+                    return NotFound();
+                
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ZipCodeDtoCreate zipCodeDtoCreate)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest();
+            
+            try
+            {
+                var result = await _service.Post(zipCodeDtoCreate);
+                if(result == null)
+                    return BadRequest();
+
+                return Created(new Uri(Url.Link("GetZipCodeWithId", new { id = result.Id })), result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] ZipCodeDtoUpdate zipCodeDtoUpdate)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            try
+            {
+                var result = await _service.Put(zipCodeDtoUpdate);
+                if(result == null)
+                    return BadRequest();
+
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
