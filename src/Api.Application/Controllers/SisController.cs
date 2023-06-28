af794dd0a58ad2def1ddd361f52cec676d24edb8
Xesquim <matheus.g.cesquim@gmail.com>
2023-06-28 00:05:46 -0300

    Adding Si controller

---
+++ src/Api.Application/Controllers/SisController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.Si;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SisController : ControllerBase
+    {
+        public ISiService _service { get; set; }
+        public SisController(ISiService service)
+        {
+            _service = service;
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
